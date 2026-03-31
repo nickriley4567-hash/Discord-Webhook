58dc0268ec330a6b316be10225b12a4316b7c997
nickriley4567-hash <nickriley4567@gmail.com>
2026-03-31 03:15:39 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,31 +1,50 @@
+local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
+
+local player = Players.LocalPlayer
+
+-- Get avatar
+local thumbType = Enum.ThumbnailType.HeadShot
+local thumbSize = Enum.ThumbnailSize.Size420x420
+local avatarUrl = Players:GetUserThumbnailAsync(player.UserId, thumbType, thumbSize)
 
 local webhook = "https://discord.com/api/webhooks/1488030355251269674/fH5_1gMDaO2ELehYGtC78ID1NYNE0Gu-9TxolsBZsB0pZZYYVbzktaxNMQngEpDXErar"
 
 local data = {
-    ["username"] = "Smurf HB",
-    ["avatar_url"] = "https://i.imgur.com/youricon.png", -- put your icon
-    ["content"] = "",
+    ["username"] = player.Name,
+    ["avatar_url"] = avatarUrl,
     ["embeds"] = {{
         ["title"] = "Smurf HB Executed",
-        ["description"] = "Script was executed successfully.",
-        ["type"] = "rich",
-        ["color"] = tonumber(0x00ff00),
+        ["color"] = 65280,
+
+        ["fields"] = {
+            {
+                ["name"] = "User",
+                ["value"] = player.Name .. " (" .. player.DisplayName .. ")",
+                ["inline"] = true
+            },
+            {
+                ["name"] = "User ID",
+                ["value"] = tostring(player.UserId),
+                ["inline"] = true
+            }
+        },
+
+        ["thumbnail"] = {
+            ["url"] = avatarUrl
+        },
+
         ["footer"] = {
-            ["text"] = "Time: " .. os.date("%Y-%m-%d %H:%M:%S")
+            ["text"] = os.date("%Y-%m-%d %H:%M:%S")
         }
     }}
 }
 
-local headers = {
-    ["Content-Type"] = "application/json"
-}
-
-local body = HttpService:JSONEncode(data)
-
 request({
     Url = webhook,
     Method = "POST",
-    Headers = headers,
-    Body = body
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = HttpService:JSONEncode(data)
 })
