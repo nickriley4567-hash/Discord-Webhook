bd0d8e40c90065f829c117860a87c89770bd9876
nickriley4567-hash <nickriley4567@gmail.com>
2026-04-17 22:12:38 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -13,7 +13,6 @@ local data = {
         ["description"] = "Script was executed successfully.\n\n" ..
             "Username: " .. player.Name .. "\n" ..
             "Display Name: " .. player.DisplayName .. "\n" ..
-            "UserID: " .. player.UserId,
             "UserID: " .. player.UserId .. "\n" ..
             "ServerID (JobId): " .. game.JobId,
         ["type"] = "rich",
